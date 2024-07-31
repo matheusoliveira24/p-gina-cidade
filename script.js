ce805d46ee71823678329a901df8c8edb8bf8468
Matheus de Oliveira Silva <159260706+matheusoliveira24@users.noreply.github.com>
2024-07-31 09:33:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+let lamp=  document.getElementById("imagem")
+let legendas= document.getElementById("legenda")
+function imagem1(){
+
+    lamp.src = '1.png'
+    legendas.innerHTML= 'Beto Carrero World'
+    
+}
+function imagem2(){
+
+    lamp. src = '2.png'
+    legendas.innerHTML= 'Praia da Joaquina'
+    
+}
+
+function imagem3(){
+
+    lamp.src = '3.png'
+    legendas.innerHTML= 'Praia da Lagoinha'
+}
+function imagem4(){
+
+    lamp. src = '4.png'
+    legendas.innerHTML= 'Vale Europeu'
+}
